7385bb3367e53afcce455b07e607d8c566467bc1
yeahbutstill <mayadani28@gmail.com>
2023-03-28 00:58:47 +0700

    test save new credit card

---
+++ src/main/resources/db/migration/card/V2__expand_columns.sql
@@ -0,0 +1,2 @@
+ALTER TABLE credit_card MODIFY COLUMN cvv VARCHAR(30);
+ALTER TABLE credit_card MODIFY COLUMN expiration_date VARCHAR(30);
--- src/test/java/guruspringframework/sdjpamultidb/SdjpaMultiDbApplicationTests.java
+++ src/test/java/guruspringframework/sdjpamultidb/SdjpaMultiDbApplicationTests.java
@@ -1,10 +1,35 @@
 package guruspringframework.sdjpamultidb;
 
+import guruspringframework.sdjpamultidb.domain.creditcard.CreditCard;
+import guruspringframework.sdjpamultidb.services.CreditCardService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SdjpaMultiDbApplicationTests {
+
+    @Autowired
+    CreditCardService creditCardService;
+
+    @Test
+    void testSaveAndGetCreditCard() {
+        CreditCard cc = CreditCard.builder()
+                .firstName("Dani")
+                .lateName("Setiawan")
+                .zipCode("12345")
+                .creditCardNumber("1234567890123456")
+                .cvv("123")
+                .expirationDate("12/20")
+                .build();
+
+        CreditCard savedCC = creditCardService.saveCreditCard(cc);
+
+        Assertions.assertNotNull(savedCC);
+        Assertions.assertNotNull(savedCC.getId());
+        Assertions.assertNotNull(savedCC.getCreditCardNumber());
+    }
 
     @Test
     void contextLoads() {
